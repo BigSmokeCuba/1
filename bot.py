a551adf6c11e7e2042b6cc45cac9c93fedfe2a4d
BigSmokeCuba <101540930+BigSmokeCuba@users.noreply.github.com>
2022-03-31 15:11:42 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,6 @@ def onmessage(update,bot:ObigramClient):
 message = bot.sendMessage(update.message.chat.id,'Procesando...')
 
 if __name__ == '__main__':
-    bot = ObigramClient('5164781245:AAGtOuaRZs2Ug0MPYUrGgNU7PB1zqFbFkuY')
+    bot = ObigramClient('5100775831:AAEBSnG12VvRAhvGt875zvpA0LyavpIyzyQ')
     bot.onMessage(onmessage)
     bot.run()
